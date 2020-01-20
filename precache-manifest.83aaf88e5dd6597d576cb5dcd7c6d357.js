b43b3a93b2f9e962822edd4ce8685e488d5fbbee
Marcos Caputo <caputo.marcos@gmail.com>
2020-01-19 22:51:12 -0300

    Updates

---
+++ precache-manifest.83aaf88e5dd6597d576cb5dcd7c6d357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15b702ffccc255b09e7dbe373a2187f",
+    "url": "/caputomarcos/my-app/index.html"
+  },
+  {
+    "revision": "fc734f2c446c86ad451a",
+    "url": "/caputomarcos/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/caputomarcos/my-app/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/caputomarcos/my-app/static/js/2.b6fabe4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc734f2c446c86ad451a",
+    "url": "/caputomarcos/my-app/static/js/main.12f0f10c.chunk.js"
+  },
+  {
+    "revision": "ca9ae61ccce692b7eb93",
+    "url": "/caputomarcos/my-app/static/js/runtime-main.f4c9a6a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/caputomarcos/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
